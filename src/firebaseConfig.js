a4b1df8cd17f18e73125fef09495d889f589058d
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-04-12 17:25:38 +0300

    add firbase config again

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyCbTGisl9Yea7qWrT97VH--NwENSqLp_Ow",
+    authDomain: "backgammon-chatter.firebaseapp.com",
+    projectId: "backgammon-chatter",
+    storageBucket: "backgammon-chatter.appspot.com",
+    messagingSenderId: "708028010012",
+    appId: "1:708028010012:web:6b34844b67f71571df19d9"
+};
