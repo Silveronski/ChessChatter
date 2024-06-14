068d095c6ec99d79275e8053f965cbb693263947
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-06-14 23:38:25 +0300

    change file extensions to ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { AuthContextProvider } from './context/AuthContext.jsx';
-import { ChatContextProvider } from './context/ChatContext.jsx';
-import { RefContextProvider } from './context/RefContext.jsx';
+import App from './App.js';
+import { AuthContextProvider } from './context/AuthContext.js';
+import { ChatContextProvider } from './context/ChatContext.js';
+import { RefContextProvider } from './context/RefContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthContextProvider>
